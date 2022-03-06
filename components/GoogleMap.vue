fe46449252920d4a070365b9abb24a25181733e5
asamiile <asamin47@gmail.com>
2022-03-06 14:41:45 +0900

    chore: install pug, add current location map

--- components/GoogleMap.vue
+++ components/GoogleMap.vue
@@ -1,20 +1,38 @@
-<template>
-  <GmapMap
-    :center="{ lat: 10, lng: 10 }"
-    :zoom="7"
+<template lang="pug">
+  GmapMap(
+    :center="center"
+    :zoom="zoom"
     map-type-id="terrain"
     style="width: 100%"
-  >
-    <GmapMarker
+  )
+    GmapMarker(
       v-for="(m, index) in markers"
       :key="index"
       :position="m.position"
       :clickable="true"
       :draggable="true"
       @click="center = m.position"
-    />
-  </GmapMap>
+    )
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 35.658600419869735, lng: 139.70182054536792 },
+      markers: [
+        {
+          position: {
+            lat: 35.658600419869735,
+            lng: 139.70182054536792,
+          },
+        },
+      ],
+      zoom: 17,
+    }
+  },
+}
+</script>
 
 <style lang="scss" scoped>
 .vue-map-container {
---
+++ components/GoogleMapCurrentLocation.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  //- 現在地を取得して地図を表示
+  GmapMap(
+    :center="center"
+    :zoom="zoom"
+    map-type-id="terrain"
+    style="width: 100%"
+  )
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 0, lng: 0 },
+      zoom: 17,
+    }
+  },
+  async mounted() {
+    const mapPosTemp = await this.getCurrentPosition()
+    const currentPos = {
+      lat: mapPosTemp.coords.latitude,
+      lng: mapPosTemp.coords.longitude,
+    }
+    this.center = currentPos
+  },
+  methods: {
+    getCurrentPosition() {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+      })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.vue-map-container {
+  height: calc(100vh - 64px);
+}
+</style>
